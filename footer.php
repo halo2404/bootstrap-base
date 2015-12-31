a07deda125d079aa200f566b83226a811a611fc3
Jens Koehler <Halo.1@gmx.net>
2015-12-31 16:03:44 +0100

    bootstrap-base
    Die Basis für Bootstrap Wordpress

---
+++ footer.php
@@ -0,0 +1,24 @@
+<!-- Hier beginnt der Fussbereich (footer.php) -->
+<div class="container-fluid" role="footer">
+    <div class="row">
+        <div class="col-sm-3">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam recusandae ducimus neque fugit facere, ex aut modi deleniti itaque non laborum odio sunt nemo illo dolores vero magni architecto aliquid.</p>
+        </div>
+        <div class="col-sm-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, aut veritatis reiciendis optio quasi magni iusto, laborum voluptatibus, explicabo possimus laboriosam culpa placeat aliquid at perferendis alias eveniet molestias deleniti.</div>
+        <div class="col-sm-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima nesciunt, suscipit sapiente natus. Error, veritatis? Odit quam eaque recusandae nisi dolorem earum enim, molestias cum nemo expedita aliquam necessitatibus facilis.</div>
+        <div class="col-sm-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam aliquam iure consequatur dolorem, nam incidunt, delectus perspiciatis dolor officia ipsam, cupiditate vero! Aliquid perferendis iure qui porro earum, dolor numquam!</div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-sm-12">Hochschulsport &middot; Abt. Karate
+            <br>Copyright &copy; 2015 &middot; All Rights Reserved &middot; fh-karate.de</div>
+    </div>
+</div>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">//, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>fh-karate.de</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <!-- Custom Style -->
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/fhkarate.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="container">
+        <nav class="navbar navbar-light navbar-full bg-faded navbar-fixed-top" role="navigation">
+            <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar2">
+                &#9776;
+            </button>
+            <div class="collapse navbar-toggleable-md" id="exCollapsingNavbar2">
+                <a class="navbar-brand" href="#">fh-karate.de</a>
+                <ul class="nav navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Karate</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#trainer">Trainer</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#zeiten">Zeiten</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#anfahrt">Anfahrt</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Bilder</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Termine</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Literatur</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Links</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#kontakt">Kontakt</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Impressum</a>
+                    </li>
+                </ul>
+        </nav>
+        </div>
+
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-sm-2 col-sm-offset-2"><img src="<?php bloginfo('template_url'); ?>/images/fh-karate-logo-final_006699_75_progressive.png" width="200" alt="Logo FH Karate"></div>
+                    <div class="col-sm-8 text-sm-center">
+                        <h1 class="display-5">Frankfurt University of Applied Sciences <small>(Fachhochschule Frankfurt am Main)</small><br>- Hochschulsport -<br>KARATE</h1></div>
+                </div>
+            </div>
+        </div>
+
+        <hr>
+        <!-- Dies ist der Kopfteil (header.php) -->
